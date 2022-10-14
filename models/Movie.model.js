27eb83b0edb9a3dd948d80052b0ffe4351809b33
Borja Cabello Luna <borjacabello@outlook.es>
2022-10-14 21:18:19 +0200

    finalized iterations and bonus

---
+++ models/Movie.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+// Schema
+const movieSchema = new mongoose.Schema({
+    title: String,
+    director: String,
+    stars: [String],
+    image: String,
+    description: String,
+    showtimes: [String]
+})
+
+// Model
+const Movie = mongoose.model("Movie", movieSchema)
+
+// Export
+module.exports = Movie
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+// Require Movie model to be able to use it on this script to get data from DB
+const Movie = require("../models/Movie.model");
+
+// GET home page
+router.get("/", (req, res, next) => res.render("index"));
+
+// GET movies page
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .select({ title: 1, image: 1 })
+    .then((moviesList) => {
+      res.render("movies.hbs", {
+        moviesList
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// GET movie details page
+router.get("/movie/:id", (req, res, next) => {
+
+    const { id } = req.params
+
+    Movie.findById(id)
+    .then((movieDetails) => {
+        res.render("details.hbs", {
+            movieDetails
+        })
+    })
+    .catch((err) =>{
+        next(err)
+    })
+})
+
 
 module.exports = router;
